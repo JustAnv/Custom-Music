e18fc6d6611fb66bf80db4f72a0817db7699c37d
JustAnv <90272587+JustAnv@users.noreply.github.com>
2021-11-05 20:08:37 +0300

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,16 +1,16 @@
 module.exports = {
-  Admins: ["668818456673779737"], //Admins of the bot
+  Admins: ["759755438459453440"], //Admins of the bot
   ExpressServer: true,//If you wanted to make the website run or not
-  DefaultPrefix: process.env.Prefix || "-", //Default prefix, Server Admins can change the prefix
+  DefaultPrefix: process.env.Prefix || "m", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/SchEbTFaNM", //Donot change..
-  Token: process.env.Token || "Your_Token_Here", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "Your_Client_Id_Here", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "Your_Client_Secret_Here", //Discord Client Secret
+  Token: process.env.Token || "OTA2MTc4NTc4MjA1NTA3NjU0.YYU2kQ.I3C_7IvE_CpbmW8Co0kdbuZArEs", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "906178578205507654", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "mUHUHMGfJAkHvAeQUNCbL9Lk41mVzn9r", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "ChangeIt", //Safety for your bot its like a password
+  CookieSecret: "Secure", //Safety for your bot its like a password
   IconURL:
     "https://media.discordapp.net/attachments/892329976114864149/892424875082407976/image0.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
@@ -29,8 +29,8 @@ module.exports = {
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "c60652f174c4462ab973acfbf650010a", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "5a417565c72545cf83711bbb84dd39f4", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "69f7e4073c6f434f8a01fcd96abf17c6", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "1a7c3b4b856247ce9a2f2c0bdb7fd478", //Spotify Client Secret
     // I have Provided a Spotify Server too. 😁
   },
 };
