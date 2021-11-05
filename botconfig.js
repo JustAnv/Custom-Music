abf4fd01374ece333f45a45cf7ef6e2916fc368b
JustAnv <90272587+JustAnv@users.noreply.github.com>
2021-11-06 00:19:38 +0300

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -3,7 +3,7 @@ module.exports = {
   ExpressServer: true,//If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || "m", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
-  SupportServer: "https://discord.gg/SchEbTFaNM", //Donot change..
+  Invite: "https://discord.com/api/oauth2/authorize?client_id=906178578205507654&permissions=8&scope=bot%20applications.commands", //Donot change..
   Token: process.env.Token, //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "906178578205507654", //Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "mUHUHMGfJAkHvAeQUNCbL9Lk41mVzn9r", //Discord Client Secret
@@ -12,7 +12,7 @@ module.exports = {
   "24/7": false, //If you want the bot to be stay in the vc 24/7
   CookieSecret: "Secure", //Safety for your bot its like a password
   IconURL:
-    "https://media.discordapp.net/attachments/892329976114864149/892424875082407976/image0.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+    "https://cdn.discordapp.com/avatars/906178578205507654/704fea6a173920caffb535f102c2063b.png?size=40", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
   Permissions: 2205280576, //Bot Inviting Permissions
   Website: process.env.Website || "http://localhost", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
